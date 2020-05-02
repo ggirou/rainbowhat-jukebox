6f21ff59e22b2f31407fd388c69ea4f2a496966d
Guillaume Girou <git@modoki.ovh>
2020-05-03 00:26:38 +0200

    Add Bluetooth class

--- jukebox.py
+++ jukebox.py
@@ -6,9 +6,11 @@ import threading
 import time
 import glob
 import colorsys
+import re
 from rainbowhat import display, lights, weather, rainbow, buzzer, touch, rainbow
 from subprocess import Popen, PIPE
 from tendo import singleton
+
 
 class Display:
   def __init__(self):
@@ -72,7 +74,7 @@ class Display:
 
 
 class Player:
-  def __init__(self, display, path='music/'):
+  def __init__(self, display, bluetooth, path='music/'):
     self.process = Popen(["echo", "Hello World!"])
     self.playing = False
     def findDir(path): return sorted(glob.glob(path + "**/", recursive=True))
@@ -82,6 +84,7 @@ class Player:
     self.currentTrack = 0
     self.macAddress = None
     self.__display = display
+    self.__bluetooth = bluetooth
     self.show()
 
     # print(self.albums)
@@ -155,14 +158,7 @@ class Player:
       print("Error %s: %s" % (returncode, error))
       if(returncode == 1):
         print("Try to reconnect")
-        if self.macAddress is None:
-          output = os.popen("echo info | bluetoothctl").read()
-          self.macAddress = output.split("Device ")[1].split(" ")[0]
-        print("Reconnect to %s" % self.macAddress)
-        cmd = "{ echo disconnect; sleep 2; echo connect '%s'; sleep 2; echo connect '%s'; sleep 2; } | bluetoothctl" % (
-            self.macAddress, self.macAddress)
-        output = os.popen(cmd).read()
-        print(output)
+        self.__bluetooth.reconnect()
 
   def pause(self):
     print("Pause")
@@ -209,9 +205,10 @@ class Player:
 
 
 class Buttons:
-  def __init__(self, display, player):
+  def __init__(self, display, player, bluetooth):
     self.display = display
     self.player = player
+    self.bluetooth = bluetooth
     self.holding = False
     touch.A.press(lambda button: self.press(button))
     touch.B.press(lambda button: self.press(button))
@@ -219,10 +216,12 @@ class Buttons:
     touch.A.release(lambda button: self.release(button))
     touch.B.release(lambda button: self.release(button))
     touch.C.release(lambda button: self.release(button))
-    self.commands = ["PLAY", "RSET", "HALT"]
+    self.commands = ["PLAY", "PAIR", "RSET", "HALT"]
     self.currentCmd = 0
+    self.holdTimer = threading.Timer(0, lambda: None)
 
   def __startHold(self, button):
+    self.holdTimer.cancel()
     self.holdTimer = threading.Timer(1.0, self.hold, [button])
     self.holdTimer.start()
 
@@ -252,6 +251,9 @@ class Buttons:
         cmd = self.commands[self.currentCmd]
         if cmd == "PLAY":
           self.player.show()
+        elif cmd == "PAIR":
+          print("Auto-Pair")
+          self.bluetooth.autopair()
         elif cmd == "RSET":
           print("Reboot")
           self.display.clear()
@@ -270,12 +272,155 @@ class Buttons:
       self.player.next()
 
 
-def main():
-  me = singleton.SingleInstance() 
+class Bluetooth:
+  def __init__(self):
+    self.device = None
 
+  def __exec(self, commands, timeouts=0, rawCommand=None):
+    if rawCommand is None:
+      if not isinstance(commands, list):
+        commands = [commands]
+      if not isinstance(timeouts, list):
+        timeouts = [timeouts] * len(commands)
+      rawCommand = ('{ %s }' % ''.join(["echo %s; sleep %s; " % (c, t) for (c, t) in zip(commands, timeouts)]))
+
+    print(rawCommand)
+    output = os.popen("%s | bluetoothctl" % rawCommand).read()
+    print(output)
+    return output
+
+  def __parseDevices(self, output):
+    return re.findall('Device ([:A-F0-9]+)', output, re.MULTILINE)
+
+  def info(self):
+    # output = os.popen("{ echo info; sleep 5; } | bluetoothctl --timeout 5").read()
+    output = self.__exec("info")
+    devices = self.__parseDevices(output)
+    self.device = next(iter(devices), None)
+    return self.device
+
+  def pairedDevices(self):
+    output = self.__exec("paired-devices")
+    return self.__parseDevices(output)
+
+  def scan(self, timeout=5):
+    output = self.__exec("scan on", timeouts=timeout)
+    return self.__parseDevices(output)
+
+  def pair(self, device, timeout=20):
+    # "scan on",
+    output = self.__exec(["scan on", "trust " + device, "pair " + device], timeouts=[0, timeout, 2])
+    trust = "trust succeeded" in output
+    pair = "Pairing successful" in output or "Failed to pair: org.bluez.Error.AlreadyExists" in output
+    print("trust %s; pair %s" % (trust, pair))
+    return trust and pair and self.connect(device)
+    # if 'Connection successful' in output:
+    #   return device
+    # else:
+    #   return None
+
+  def unpair(self, device, timeout=2):
+    output = self.__exec(["untrust " + device, "remove " + device], timeouts=timeout)
+    untrust = "untrust succeeded" in output
+    remove = "remove succeeded" in output
+    print("untrust %s; remove %s" % (untrust, remove))
+
+  def autopair(self, timeout=5, tries=3, pairedDevices=None):
+    pass
+
+  def autoconnect(self, timeout=5, tries=3, pairedDevices=None):
+    print("Autoconnect timeout=%s, tries=%s" % (timeout, tries))
+    currentDevice = self.info()
+    if currentDevice is not None:
+      print("Already connected to %s" % currentDevice)
+      return currentDevice
+
+    if pairedDevices is None:
+      pairedDevices = self.pairedDevices()
+    print("pairedDevices: %s" % pairedDevices)
+    # scannedDevices = self.scan(timeout=timeout)
+    # connectableDevices = set(pairedDevices) & set(scannedDevices)
+    # print("scannedDevices: %s" % scannedDevices)
+    # print("connectableDevices: %s" % connectableDevices)
+    for device in pairedDevices:
+      result = self.connect(device)
+      if result is not None:
+        return result
+    # if len(pairedDevices) > 0:
+    #   return self.connect(pairedDevices[1])
+    # elif len(connectableDevices) > 0:
+    #   return self.connect(connectableDevices.pop())
+    # elif tries > 0:
+    #   return self.autoconnect(timeout=timeout*2, tries=tries-1, pairedDevices=pairedDevices)
+    # else:
+    #   return None
+
+  def connect(self, device, timeout=2):
+    output = self.__exec("connect %s" % device, timeouts=timeout)
+    if 'Connection successful' in output:
+      self.device = device
+      return device
+    else:
+      return None
+
+  def disconnect(self, device='', timeout=1):
+    if device == '' and self.device is not None:
+      device = self.device
+    self.__exec("disconnect %s" % device, timeouts=timeout)
+    self.device = None
+
+  def reconnect(self):
+    self.disconnect()
+    return self.autoconnect()
+    # print("Try to reconnect")
+    # if self.device is None:
+    #   output = os.popen("echo info | bluetoothctl").read()
+    #   self.device = output.split("Device ")[1].split(" ")[0]
+    # print("Reconnect to %s" % self.device)
+    # cmd = "{ echo disconnect; sleep 2; echo connect '%s'; sleep 2; echo connect '%s'; sleep 2; } | bluetoothctl" % (
+    #     self.device, self.device)
+    # output = os.popen(cmd).read()
+    # print(output)
+
+
+def main():
+  # bt = Bluetooth()
+  # mac = '78:44:05:96:3D:EE'
+
+  # device = bt.unpair(mac)
+  # print(device)
+
+  # device = bt.pair(mac)
+  # print(device)
+
+  # device = bt.autopair()
+  # print(device)
+
+  # mac = bt.info()
+  # print(mac)
+
+  # devices = bt.pairedDevices()
+  # print(devices)
+
+  # devices = bt.scan()
+  # print(devices)
+
+  # device = bt.disconnect()
+  # print(device)
+
+  # device = bt.connect('78:44:05:96:3D:EE')
+  # print(device)
+
+  # device = bt.reconnect()
+  # print(device)
+
+  # singleton.SingleInstance()
+
+  bluetooth = Bluetooth()
   with Display() as display:
-    with Player(display) as player:
-      Buttons(display, player)
+    with Player(display, bluetooth) as player:
+      Buttons(display, player, bluetooth)
+      bluetooth.autoconnect()
       signal.pause()
 
 
